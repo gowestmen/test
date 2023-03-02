692ab7055b319a91b74d6dfc220a7be3a2cedfc5
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-03-02 22:22:58 +0100

    création maps_ext_1_fond_test

---
+++ tilesets/maps_ext_1_fond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="maps_ext_1_fond" tilewidth="35" tileheight="35" tilecount="572" columns="26">
+ <image source="E:/cours/Atelier pro/campusentier.JPG" width="928" height="798"/>
+</tileset>
